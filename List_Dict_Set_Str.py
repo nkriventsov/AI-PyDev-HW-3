a239ef9ec94dcb2f371ca4da83201003eb4fce06
nkriventsov <61682500+nkriventsov@users.noreply.github.com>
2022-11-01 14:13:20 +0300

    Initial commit

---
+++ List_Dict_Set_Str.py
@@ -0,0 +1,16 @@
+# 1. Из всех методов списка (list) выбрать 5 тех, которые по вашему мнению используются чаще всего
+# 2. Написать их через запятую с параметрами
+# 3. Повторить процедуру для словарей (dict), множеств (set), строк (str)
+# Задание творческое правильного ответа нет, предлагаю логически подумать какие методы наиболее популярные
+#______________________________________________________________________________________
+
+# list: list.append(x), list.remove(x), list.count(x), list.index(x, [start [, end]]), list.clear()
+# dict: dict.get(key[, default]), dict.items(), dict.keys(), dict.values(), dict.update([other])
+# set: len(s), x in s, set.add(elem), set.discard(elem), set.difference_update(other, ...); set -= other | ..., set.intersection_update(other, ...); set &= other & ...
+# str: len(S), S.find(str, [start],[end]), S[i:j:step], S.replace(шаблон, замена[, maxcount]), S.format(*args, **kwargs)
+
+
+
+
+
+
